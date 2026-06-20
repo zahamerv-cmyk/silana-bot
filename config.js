f551bdd2b207ff9c2e2e26a7e2dd8e71a4265c10
zahamerv-cmyk <zahamerv@gmail.com>
2026-06-20 18:21:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.owner = [
 
 global.info = {
  nomerbot: '212605784394',
- pairingNumber: '212693227969',
+ pairingNumber: '212627278517',
  nameown: 'Nour',
  nomerown: '212717457920',
  packname: 'sticker by ',
